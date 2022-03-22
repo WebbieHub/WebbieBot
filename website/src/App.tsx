238dbb81f4602baff682e32570a7f331d795b8a4
Julien <jbertazzolambert@gmail.com>
2022-03-22 12:38:45 -0400

    style: some sizing

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,6 +1,7 @@
 import {
   ChakraProvider,
   theme,
+  Box,
 } from "@chakra-ui/react"
 import { Routes, Route } from 'react-router-dom'
 import Nav from "./components/Nav"
@@ -11,10 +12,12 @@ import Admin from "./views/Admin"
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Nav />
-    <Routes>
-      <Route path="/" element={<About />} />
-      <Route path="/stats" element={<Stats />} />
-      <Route path="/admin" element={<Admin />} />
-    </Routes>
+    <Box display="grid" placeItems="center" w="full" h="full" m={4} overflow="hidden">
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Box>
   </ChakraProvider>
 )
