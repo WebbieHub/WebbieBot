9ad4f6affd28d7a2c0f674b34c6ec2ab6c8bf3d9
judy-n <judynaamani@icloud.com>
2022-03-27 11:36:45 -0400

    feat: color mode default to system

--- website/src/theme.ts
+++ website/src/theme.ts
@@ -1,4 +1,9 @@
-import { extendTheme} from "@chakra-ui/react";
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+    useSystemColorMode: true
+}
+
 
 const theme = extendTheme({
     fonts: {
@@ -13,7 +18,8 @@ const theme = extendTheme({
                 }
             }
         }
-    }
+    },
+    config
 
 })
 
